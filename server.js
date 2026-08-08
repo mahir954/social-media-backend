50cf7fc7f12948ffea31bdd8787b28a4ac7bb858
mahir954 <mk0914509@gmail.com>
2026-08-08 07:05:18 -0600

    Update server.js

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ socket.on("end-call", (data) => {
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, "0.0.0.0", () => {
-    console.log(Server is running on port ${PORT});
+    console.log(`Server is running on port ${PORT}`);
 });
 
 
