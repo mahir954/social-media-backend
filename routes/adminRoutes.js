d372098ce727c6ed4858b90d2bd9c145c736ff7a
mahir954 <mk0914509@gmail.com>
2026-08-12 05:29:23 -0600

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,4 +1,4 @@
-const Admin = require("../models/Admin");
+
 const adminMiddleware = require("../middleware/adminMiddleware");
 const express = require("express");
 const bcrypt = require("bcryptjs");
